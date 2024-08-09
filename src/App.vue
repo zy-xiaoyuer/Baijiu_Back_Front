3480159e5a5bb0555d5ed8518cfccf560ec928ae
tingting <3366211397@qq.com>
2024-08-09 20:40:16 +0800

    login

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <nav>
+    <router-link to="/login">Login</router-link> |
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="less">
